323da8f185144fd95a5ffe86873c7c0a217ef96f
Nhị Vĩ <nguyenlenghia.it@gmail.com>
2019-01-10 17:25:59 +0700

    add website1 + ssoowin

---
+++ SSOOwinCookieAuthentication.Website1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SSOOwinCookieAuthentication.Website1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SSOOwinCookieAuthentication.Website1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SSOOwinCookieAuthentication.Website1.WebApiApplication" Language="C#" %>
