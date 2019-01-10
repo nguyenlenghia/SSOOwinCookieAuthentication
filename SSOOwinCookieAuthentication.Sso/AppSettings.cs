f51fc2a3017e5f5f230718ce69480c61b0af8f3e
Nhị Vĩ <nguyenlenghia.it@gmail.com>
2019-01-10 18:34:43 +0700

    add sso way 1. use dll

---
+++ SSOOwinCookieAuthentication.Sso/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SSOOwinCookieAuthentication.Sso
+{
+    public class AppSettings
+    {
+        internal static readonly string LoginPath = "/Account/Login";
+        internal static readonly string LogoutPath = "/Account/Logout";
+
+        internal static string AuthenticationType => ConfigurationManager.AppSettings["AppAuthenticationType"] ?? "SsoCookies";
+    }
+}
---
+++ SSOOwinCookieAuthentication.Sso/App_Start/Startup.Auth.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using SSOOwinCookieAuthentication.SsoSecurity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSOOwinCookieAuthentication.Sso
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Enable the Application Sign In Cookie.
+            // UseCookieAuthentication is an extension of Microsoft.Owin.Security.Cookies.CookieAuthenticationExtensions
+            app.UseCookieAuthentication(new SsoCookieAuthenticationOptions
+            {
+                AuthenticationType = SsoSettings.AuthenticationType,
+
+                // The value of CookieName and CookieDomain must is the same in every sso website.
+                CookieName = SsoSettings.CookieName,
+                CookieDomain = SsoSettings.CookieDomain,
+            });
+
+            // Set default Way to SignIn as AuthenticationType
+            app.SetDefaultSignInAsAuthenticationType(AppSettings.AuthenticationType);
+        }
+    }
+}
